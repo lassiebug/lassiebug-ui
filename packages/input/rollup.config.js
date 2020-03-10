a3df7b71e52b43a5a938fde1f9ac164f4806f5eb
Erik Badger <erikbadger777@gmail.com>
2020-03-10 16:43:54 -0500

    added initail packages

---
+++ packages/input/rollup.config.js
@@ -0,0 +1,24 @@
+
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+
+export default [
+    {
+        input: "src/index.tsx",
+        external: Object.keys(pkg.peerDependencies || {}),
+        plugins: [
+            typescript({
+                typescript: require("typescript")
+            })
+        ],
+        output: [
+            { file: pkg.main, format: "cjs" },
+            { file: pkg.module, format: "esm" },
+            // {
+            //     file: "example/src/reactComponentLib/index.js",
+            //     format: "es",
+            //     banner: "/* eslint-disable */"
+            // }
+        ]
+    }
+];
---
+++ packages/input/src/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Test = () => (<h1>This is a test</h1>);
