648d526b0b17e187e2d7aa3269cac05163ee8811
Erik Badger <erikbadger777@gmail.com>
2020-07-02 10:16:57 -0500

    added button?

---
+++ packages/input/src/components/buttons/index.tsx
@@ -0,0 +1,72 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+
+
+type Sizes = 'sm' | 'md' | 'lg';
+type Types = 'primary' | 'danger' | 'success' | 'warning';
+
+interface ButtonProps {
+    size:Sizes;
+    type:Types;
+    value:string;
+    flat:boolean;
+}
+
+const get_color = (color?:Types) => {
+    if(!color)return 'var(--primary)';
+    return `var(--${color})`;
+}
+
+const StyledButton = styled.button`
+
+    border:${(props:ButtonProps) => {
+        if(props.flat) {
+            return `2px solid ${get_color(props.type)}`
+        } else {
+            return 'none';
+        }
+    }};
+    background-color:${(props:ButtonProps) => {
+        if(props.flat) {
+            return 'none';
+        } else {
+            return get_color(props.type);
+        }
+
+    }};
+
+    padding:.5rem 1rem;
+    border-radius:5px;
+
+    color:var(--black);
+
+    cursor: pointer;
+    transition:var(--shadow-transition), transform .2s;
+
+    ${(props:ButtonProps) => {
+        if(props.flat)return 'none';
+        return `
+            box-shadow:var(--shadow-00);
+            &:hover {
+                box-shadow:var(--shadow-10);
+            }
+
+            &:target, &:active {
+                box-shadow:var(--shadow-00);
+                transform:translate3d(0, 2px, 0);
+            }
+        `;
+    }};
+
+`;
+
+const Button:FC<ButtonProps> = (props) => {
+    return (
+        <StyledButton {...props}>
+            { props.value }
+            { props.children }
+        </StyledButton>
+    )
+}
+
+export default Button;
